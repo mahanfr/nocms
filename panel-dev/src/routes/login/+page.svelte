03d4a86df7bbeb9d213462f1bfaa81e5cfa601fe
mahanfarzaneh2000 <mahan.farzaneh@gmail.com>
2023-01-31 18:55:03 +0330

    adding panel dev with svelte

---
+++ panel-dev/src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Login</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		This is a login page
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+</section>
